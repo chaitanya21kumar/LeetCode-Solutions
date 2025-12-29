0e12de267b00bb4c598e912abeb5aabeeb4ea359
Chaitanya Kumar <multiversesyndrome@gmail.com>
2025-12-29 19:32:01 +0530

    Time: 0 ms (100.00%) | Memory: 19.1 MB (96.97%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
+    int dp[250][250];
     int dr[4]={-1,0,1,0};
     int dc[4]={0,1,0,-1};
-    int res=1;
-    int dp[250][250];
-    int dfs(int i,int j,int m,int n,vector<vector<int>>& matrix,int t){
+    int dfs(int i,int j,int m,int n,vector<vector<int>>& matrix){
+
         if(dp[i][j]!=-1) return dp[i][j];
+
         int ans=1;
+
         for(int k=0;k<4;k++){
             int r=i+dr[k];
             int c=j+dc[k];
-            int len=1;
+
             if(r>=0 && r<m && c>=0 && c<n && matrix[r][c]>matrix[i][j]){
-                len=1+dfs(r,c,m,n,matrix,t+1);
+                int len=1+dfs(r,c,m,n,matrix);
+                ans=max(ans,len);
             }
-            ans=max(ans,len);
+
         }
 
         return dp[i][j]=ans;
@@ -23,12 +26,11 @@ public:
     int longestIncreasingPath(vector<vector<int>>& matrix) {
 
         int m=matrix.size(),n=matrix[0].size();
-        memset(dp,-1,sizeof(dp));
         int ans=1;
+        memset(dp,-1,sizeof(dp));
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                int temp=1;
-                ans=max(ans,dfs(i,j,m,n,matrix,temp));
+                ans=max(ans,dfs(i,j,m,n,matrix));
             }
         }
 
