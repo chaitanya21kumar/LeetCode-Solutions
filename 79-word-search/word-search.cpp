1c59ae1d84cf305b98dd0e763e2f53277b87bd6a
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-02 23:27:23 +0530

    Time: 189 ms (86.63%) | Memory: 10.3 MB (93.62%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,43 +1,36 @@
 class Solution {
-public:
+public: 
     int dr[4]={-1,0,1,0};
     int dc[4]={0,1,0,-1};
-
-    int dfs(int i,int j,int k,int ws,int n,int m,vector<vector<char>>& board,string &word,vector<vector<int>> &vis){
-        vis[i][j]=1;
+    int dfs(int i,int j,int k,int ws,int m,int n,vector<vector<char>>& board,string &word){
         if(k==ws) return true;
-        
+        char org=board[i][j];
+        board[i][j]='#';
 
         for(int l=0;l<4;l++){
             int r=i+dr[l];
             int c=j+dc[l];
-            if(r>=0 && r<n && c>=0 && c<m && board[r][c]==word[k] && !vis[r][c]){
-                if(dfs(r,c,k+1,ws,n,m,board,word,vis)) return true;
+            if(r>=0 && r<m && c>=0 && c<n && board[r][c]==word[k] && board[r][c]!='#'){
+                if(dfs(r,c,k+1,ws,m,n,board,word)) return true;
             }
         }
-        vis[i][j]=0;
 
+        board[i][j]=org;
         return false;
-
-
     }
     bool exist(vector<vector<char>>& board, string word) {
 
-        int n=board.size();
-        int m=board[0].size();
-        int ws=word.size(); // word size
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                vector<vector<int>> vis(n,vector<int> (m,0));
+        int m=board.size(),n=board[0].size(),ws=word.size();
+
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
                 if(board[i][j]==word[0]){
-                    if(dfs(i,j,1,ws,n,m,board,word,vis)) return true;
+                    if(dfs(i,j,1,ws,m,n,board,word)) return true;
                 }
             }
         }
 
         return false;
-
         
     }
 };
