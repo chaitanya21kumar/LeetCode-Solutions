be32bd225c590c107acac9c0a2390206ed921acb
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-04 11:31:58 +0530

    Time: 0 ms (100.00%) | Memory: 21.8 MB (45.26%) - LeetSync

--- 57-insert-interval/insert-interval.cpp
+++ 57-insert-interval/insert-interval.cpp
@@ -1,10 +1,9 @@
 class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
-        int n=intervals.size();
-        vector<vector<int>> ans;
 
-        int i=0;
+        int n=intervals.size(),i=0;
+        vector<vector<int>> ans;
         while(i<n && newInterval[0]>intervals[i][1]) ans.push_back(intervals[i++]);
         while(i<n && newInterval[1]>=intervals[i][0]){
             newInterval[0]=min(newInterval[0],intervals[i][0]);
@@ -13,9 +12,8 @@ public:
         }
         ans.push_back(newInterval);
         while(i<n) ans.push_back(intervals[i++]);
-
         return ans;
-
+        
         
     }
 };
