94fb538a7d43452afee85ea79e333b8c88740024
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-04 15:25:48 +0530

    Time: 4 ms (67.33%) | Memory: 23.9 MB (59.24%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+
+        int n=intervals.size();
+        vector<vector<int>> ans;
+        sort(intervals.begin(),intervals.end());
+        for(int i=0;i<n;i++){
+            if(ans.empty() || intervals[i][0]>ans.back()[1]) ans.push_back(intervals[i]);
+            else if(intervals[i][0]<=ans.back()[1]){
+                ans.back()[1]=max(intervals[i][1],ans.back()[1]);
+            }
+        }
+
+        return ans;
+        
+
+
+        
+    }
+};
