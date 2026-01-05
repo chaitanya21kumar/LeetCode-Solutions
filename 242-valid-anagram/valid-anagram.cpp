8727786206f6999d9803e46d68c32ed63af60995
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-05 10:25:24 +0530

    Time: 3 ms (48.20%) | Memory: 9.8 MB (25.54%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -2,9 +2,16 @@ class Solution {
 public:
     bool isAnagram(string s, string t) {
 
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-        return s==t;
+        vector<int> v(26,0);
+        if(s.length()!=t.length()) return false;
+        for(int i=0;i<s.size();i++){
+            v[s[i]-'a']++;
+            v[t[i]-'a']--;
+        }
+        for(int i=0;i<26;i++){
+            if(v[i]!=0) return false;
+        }
+        return true;
         
     }
 };
