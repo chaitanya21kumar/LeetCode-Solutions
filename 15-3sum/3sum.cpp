b5b84a091caa90215cafb7963f44be3fccedbba7
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-06 12:44:22 +0530

    Time: 44 ms (75.36%) | Memory: 29.2 MB (33.68%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -6,12 +6,12 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(),nums.end());
         for(int i=0;i<n;i++){
-            int l=i+1,r=n-1;
             if(i>0 && nums[i]==nums[i-1]) continue;
+            int l=i+1,r=n-1;
             while(l<r && r<n){
-                int s=nums[i]+nums[l]+nums[r];
-                if(s>0) r--;
-                else if(s<0) l++;
+                int sum=nums[i]+nums[l]+nums[r];
+                if(sum>0) r--;
+                else if(sum<0) l++;
                 else{
                     ans.push_back({nums[i],nums[l],nums[r]});
                     l++;
@@ -19,8 +19,9 @@ public:
                 }
             }
         }
+
         return ans;
-        // -4 -1 -1 0 1 2 
+
         
     }
 };
