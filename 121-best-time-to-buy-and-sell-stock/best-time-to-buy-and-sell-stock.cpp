9e1d9553ec35ea67b2d632e71e0892feac7c0a08
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-06 23:22:23 +0530

    Time: 0 ms (100.00%) | Memory: 97.3 MB (59.89%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int ans=0,n=prices.size();
-        int amt=prices[0];
+        int n=prices.size();
+        int b=0;
+        int mn=prices[0];
+        int ans=0;
         for(int i=1;i<n;i++){
-            if(prices[i]<amt){
-                amt=prices[i];
-            }
-            else if(prices[i]>amt){
-                ans=max(ans,prices[i]-amt);
-            }
+            if(prices[i]>mn) ans=max(ans,prices[i]-mn);
+            mn=min(mn,prices[i]);
         }
-
         return ans;
         
     }
