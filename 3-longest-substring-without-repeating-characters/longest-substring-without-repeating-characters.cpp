bb449fc06407e3ccd7f15e9eb9b80d83c0ee3762
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-06 23:53:14 +0530

    Time: 655 ms (5.03%) | Memory: 247.5 MB (5.03%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -5,7 +5,7 @@ public:
         int n=s.size();
         int ans=0;
         for(int i=0;i<n;i++){
-            set<char> st;
+            unordered_set<char> st;
             int c=0;
             for(int j=i;j<n;j++){
                 if(st.find(s[j])!=st.end()){
