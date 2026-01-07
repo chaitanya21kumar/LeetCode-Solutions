ca51b2aaa9d4be248018c8abe6d77aab33c01d6d
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-07 13:50:09 +0530

    Time: 23 ms (18.02%) | Memory: 16.9 MB (9.65%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -3,29 +3,18 @@ public:
     int lengthOfLongestSubstring(string s) {
 
         int n=s.size();
-        int l=0,r=0;
-        map<int,int> m;
-        int mx=0;
-        int c=0;
+        set<char> st;
+        int l=0,r=0,ans=0;
         while(r<n){
-            if(m[s[r]]==0){
-                m[s[r]]++;
-                c++;
+            while(st.find(s[r])!=st.end()){
+                st.erase(s[l]);
+                l++;
             }
-            else{
-                while(m[s[r]]!=0){
-                    m[s[l]]--;
-                    if(m[s[l]]==0) m.erase(s[l]);
-                    l++;
-                    c--;
-                }
-                m[s[r]]++;
-                c++;
-            }
-            mx=max(mx,c);
+            st.insert(s[r]);
+            ans=max(ans,r-l+1);
             r++;
         }
-        return mx;
+        return ans;
         
     }
 };
