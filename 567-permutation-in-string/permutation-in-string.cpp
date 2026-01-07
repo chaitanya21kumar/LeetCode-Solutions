b23483bc7d76eb93d49bc9e41ce54dae4501d1a9
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-07 18:27:48 +0530

    Time: 597 ms (7.77%) | Memory: 84.7 MB (5.01%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -3,11 +3,11 @@ public:
     bool checkInclusion(string s1, string s2) {
 
         int n1=s1.size(),n2=s2.size();
-        map<char,int> m;
+        unordered_map<char,int> m;
         for(int i=0;i<n1;i++) m[s1[i]]++;
 
         for(int i=0;i<=n2-n1;i++){
-            map<char,int> m2;
+            unordered_map<char,int> m2;
             for(int j=i;j<=i+n1-1;j++){
                 m2[s2[j]]++;
             }
