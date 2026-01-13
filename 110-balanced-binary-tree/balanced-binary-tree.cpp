393cf2fb67807100685650d485eba1daf928c5ff
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-13 19:34:50 +0530

    Time: 3 ms (11.34%) | Memory: 23.1 MB (21.60%) - LeetSync

--- 110-balanced-binary-tree/balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/balanced-binary-tree.cpp
@@ -11,23 +11,18 @@
  */
 class Solution {
 public:
-    int f1(TreeNode* root){
+    int f(TreeNode* root){
         if(!root) return 0;
-        int lh=1+f1(root->left);
-        int rh=1+f1(root->right);
-
-        return max(lh,rh);
-    }
-    bool f(TreeNode* root){
-        if(!root) return true;
-        int lh=f1(root->left);
-        int rh=f1(root->right);
-        if(abs(rh-lh)>1) return false;
-        return f(root->left) && f(root->right);
+        int lh=f(root->left);
+        if(lh==-1) return -1;
+        int rh=f(root->right);
+        if(rh==-1) return -1;
+        if(abs(rh-lh)>1) return -1;
+        return 1+max(lh,rh);
     }
     bool isBalanced(TreeNode* root) {
 
-        return f(root);
+        return f(root)!=-1;
         
     }
 };
