62c5909abc0efb8afb1d39cd104473db0b5737e5
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-21 15:40:52 +0530

    Time: 11 ms (32.53%) | Memory: 27 MB (58.55%) - LeetSync

--- 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.cpp
+++ 689-maximum-sum-of-3-non-overlapping-subarrays/maximum-sum-of-3-non-overlapping-subarrays.cpp
@@ -2,48 +2,45 @@ class Solution {
 public:
     vector<int> ans;
     vector<int> pref;
-    int dp[30000][5];
-    int f1(int i,int c,int k,int n,vector<int> &nums){
+    int dp[20010][5];
+    int f1(int i,int c,int k,int n){
         if(c==0) return 0;
         if(i>=n) return INT_MIN;
-        int l=i,r=i+k-1;
-        int sum=0;
         if(dp[i][c]!=-1) return dp[i][c];
+        int l=i,r=i+k-1,sum=0;
         if(r<n){
-            sum=pref[r]-(l>0 ? pref[l-1]:0);
+            sum=pref[r]-(l>0?pref[l-1]:0);
         }
-        int take=sum+f1(i+k,c-1,k,n,nums);
-        int nottake=f1(i+1,c,k,n,nums);
+        int take=sum+f1(i+k,c-1,k,n);
+        int nottake=f1(i+1,c,k,n);
         return dp[i][c]=max(take,nottake);
     }
-    int f(int i,int c,int k,int n,vector<int>& nums){
+    int f(int i,int c,int k,int n){
         if(c==0) return 0;
         if(i>=n) return INT_MIN;
         int l=i,r=i+k-1;
         int sum=0;
         if(r<n){
-            sum=pref[r]-(l>0 ? pref[l-1]:0);
+            sum=pref[r]-(l>0?pref[l-1]:0);
         }
-        int take=sum+f1(i+k,c-1,k,n,nums);
-        int nottake=f1(i+1,c,k,n,nums);
+        int take=sum+f1(i+k,c-1,k,n);
+        int nottake=f1(i+1,c,k,n);
         if(take>=nottake){
             ans.push_back(i);
-            f(i+k,c-1,k,n,nums);
+            f(i+k,c-1,k,n);
         }
-        else f(i+1,c,k,n,nums);
+        else f(i+1,c,k,n);
         return max(take,nottake);
     }
     vector<int> maxSumOfThreeSubarrays(vector<int>& nums, int k) {
 
         int n=nums.size();
-        pref.assign(n,0);
+        pref.assign(n+1,0);
         pref[0]=nums[0];
         memset(dp,-1,sizeof(dp));
-        for(int i=1;i<n;i++){
-            pref[i]=pref[i-1]+nums[i];
-        }
-        f(0,3,k,n,nums);
+        for(int i=1;i<n;i++) pref[i]=pref[i-1]+nums[i];
+
+        f(0,3,k,n);
         return ans;
-        
     }
 };
