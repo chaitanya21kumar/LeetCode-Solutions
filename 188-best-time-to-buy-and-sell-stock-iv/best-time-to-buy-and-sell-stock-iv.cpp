4da90db7e1c4ae063c27a207065f3e9ea0ca1c04
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-21 10:12:40 +0530

    Time: 23 ms (16.61%) | Memory: 16.9 MB (59.19%) - LeetSync

--- 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
+++ 188-best-time-to-buy-and-sell-stock-iv/best-time-to-buy-and-sell-stock-iv.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int f(int i,int b,int k,int n,vector<int> &prices,vector<vector<vector<int>>> &dp){
-        if(i>=n || k<=0) return 0;
-        if(dp[i][b][k]!=-1) return dp[i][b][k];
-        int buy=0,notbuy=0,sell=0,notsell=0;
+    int dp[1001][101][3];
+    int f(int i,int b,int k,int n,vector<int>& prices){
+        if(i>=n || k==0) return 0;
+        if(dp[i][k][b]!=-1) return dp[i][k][b];
+        int buy=0,sell=0,skip=0;
         if(b){
-            buy=-prices[i]+f(i+1,0,k,n,prices,dp);
-            notbuy=f(i+1,b,k,n,prices,dp);
-            dp[i][b][k]=max(buy,notbuy);
+            buy=-prices[i]+f(i+1,0,k,n,prices);
         }
-        else{
-            sell=prices[i]+f(i+1,1,k-1,n,prices,dp);
-            notsell=f(i+1,0,k,n,prices,dp);
-            dp[i][b][k]=max(dp[i][b][k],max(sell,notsell));
+        if(!b){
+            sell=+prices[i]+f(i+1,1,k-1,n,prices);
         }
-
-        return dp[i][b][k];
+        skip=f(i+1,b,k,n,prices);
+        return dp[i][k][b]=max({buy,sell,skip});
     }
     int maxProfit(int k, vector<int>& prices) {
-
+        memset(dp,-1,sizeof(dp));
         int n=prices.size();
-        vector<vector<vector<int>>> dp(n,vector<vector<int>> (2,vector<int> (k+1,-1)));
-
-        return f(0,1,k,n,prices,dp);
-        
+        return f(0,1,k,n,prices);
     }
 };
