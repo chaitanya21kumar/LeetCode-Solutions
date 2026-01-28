5e2fe3c378d06ba44aa50c74c242d8225dd60f2f
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-28 15:45:37 +0530

    Time: 0 ms (100.00%) | Memory: 10.1 MB (36.34%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     vector<vector<int>> ans;
-    void f(int i,int n,vector<int>& nums,vector<int> temp){
+    void f(int i,int n,vector<int>& nums,vector<int>& temp){
         if(i>=n){
             ans.push_back(temp);
             return;
         }
+
         temp.push_back(nums[i]);
         f(i+1,n,nums,temp);
         temp.pop_back();
         f(i+1,n,nums,temp);
-        return;
+
+
     }
     vector<vector<int>> subsets(vector<int>& nums) {
 
@@ -18,7 +20,6 @@ public:
         vector<int> temp;
         f(0,n,nums,temp);
         return ans;
-
         
     }
 };
