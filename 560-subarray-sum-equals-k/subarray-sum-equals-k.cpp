e87415b2df0bb14a19bd869d1f9827f622b616ad
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-30 20:47:44 +0530

    Time: 67 ms (32.39%) | Memory: 47.8 MB (24.98%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+
+        int n=nums.size();
+        int l=0,r=0,s=0;
+        map<int,int> m;
+        int ans=0;
+        m[0]=1;
+        while(r<n){
+            s+=nums[r];
+            if(m.count(s-k)){
+                ans+=m[s-k];
+            }
+            m[s]++;
+            r++;
+        }
+        return ans;
+
+    }
+};
