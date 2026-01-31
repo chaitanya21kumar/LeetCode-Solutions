dfaa1c23a8332c9588c15fb03e363436333a02fc
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-01-31 15:47:02 +0530

    Time: 70 ms (77.41%) | Memory: 88.9 MB (63.22%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -3,10 +3,10 @@ public:
     int longestConsecutive(vector<int>& nums) {
 
         int n=nums.size();
+        int ans=0;
         unordered_set<int> s;
         for(int i=0;i<n;i++) s.insert(nums[i]);
-        int ans=0;
-        for(auto x:s){
+        for(auto &x:s){
             if(s.find(x-1)==s.end()){
                 int l=0;
                 while(s.find(x+l)!=s.end()){
