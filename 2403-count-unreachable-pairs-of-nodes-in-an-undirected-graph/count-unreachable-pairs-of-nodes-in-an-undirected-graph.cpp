88beecb8bfb8c09b5431e25c61a746555d24ecb2
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-01 09:50:47 +0530

    Time: 161 ms (36.13%) | Memory: 206.9 MB (18.28%) - LeetSync

--- 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
+++ 2403-count-unreachable-pairs-of-nodes-in-an-undirected-graph/count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -1,34 +1,38 @@
 class Solution {
 public:
-    unordered_map<int,vector<int>> adj;
-    vector<int> vis;
     typedef long long int ll;
-    void dfs(ll i,ll &s){
-        vis[i]=1;
-        s++;
-        for(auto &x:adj[i]){
-            if(!vis[x]) dfs(x,s);
-        }
-    }
-    long long countPairs(int n, vector<vector<int>>& edges) {
+    void dfs(int i,int n,int &cs,vector<int> &vis){
 
-        vis.assign(n+1,0);
+        vis[i]=1;
+        cs++;
+        for(auto &x:adj[i]){
+            if(!vis[x]) dfs(x,n,cs,vis);
+        }
+
+    }
+    unordered_map<int,vector<int>> adj;
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        adj.clear();
         for(auto &x:edges){
             int u=x[0];
             int v=x[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-        ll cur_rem=n;
         ll ans=0;
+        int tc=n;
+        vector<int> vis(n,0);
+
         for(int i=0;i<n;i++){
+            int cs=0;
             if(!vis[i]){
-                ll s=0;
-                dfs(i,s);
-                ans+=(s*(cur_rem-s));
-                cur_rem-=s;
+                dfs(i,n,cs,vis);
             }
+            ans+=((ll)cs*(tc-cs));
+            tc-=cs;
         }
         return ans;
+
+        
     }
 };
