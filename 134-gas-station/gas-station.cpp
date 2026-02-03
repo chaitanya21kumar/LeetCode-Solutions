252eb5944758b08cf051e71a1b2941b902fe82e9
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-03 22:31:57 +0530

    Time: 0 ms (100.00%) | Memory: 112.4 MB (85.18%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+
         int n=gas.size();
         int cg=0,cc=0;
         for(int i=0;i<n;i++){
@@ -8,14 +9,14 @@ public:
             cc+=cost[i];
         }
         if(cc>cg) return -1;
-        int sidx=0;
         int s=0;
+        int sidx=0;
         for(int i=0;i<n;i++){
             s+=gas[i];
             s-=cost[i];
             if(s<0){
-                sidx=i+1;
                 s=0;
+                sidx=i+1;
             }
         }
         return sidx;
