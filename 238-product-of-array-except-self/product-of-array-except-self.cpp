4053ead58108164207614b2a50b2af2aff0c93ea
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-04 13:48:03 +0530

    Time: 5 ms (8.77%) | Memory: 42.2 MB (25.05%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
-        int p=1,s=1,n=nums.size();
-        vector<int> ans(n);
+        int n=nums.size();
+        vector<int> pp(n,1),sp(n,1),ans(n);
+        for(int i=1;i<n;i++){
+            pp[i]=pp[i-1]*nums[i-1];
+        }
+        for(int i=n-2;i>=0;i--){
+            sp[i]=sp[i+1]*nums[i+1];
+        }
         for(int i=0;i<n;i++){
-            ans[i]=p;
-            p*=nums[i];
+            ans[i]=pp[i]*sp[i];
         }
-
-        for(int i=n-1;i>=0;i--){
-            ans[i]*=s;
-            s*=nums[i];
-        }
-
         return ans;
-        
     }
 };
