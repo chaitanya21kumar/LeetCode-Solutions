5c86a0a4aa5a57c73a8c844ec8661c3c84f0c2cb
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-07 00:33:53 +0530

    Time: 0 ms (100.00%) | Memory: 10.5 MB (69.29%) - LeetSync

--- 198-house-robber/house-robber.cpp
+++ 198-house-robber/house-robber.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
-    int dp[110];
+    vector<int> dp;
     int f(int i,int n,vector<int>& nums){
         if(i>=n) return 0;
         if(dp[i]!=-1) return dp[i];
-        int pick=0,notpick=0;
+        int pick=0;
+        int notpick=0;
         pick=nums[i]+f(i+2,n,nums);
         notpick=f(i+1,n,nums);
         return dp[i]=max(pick,notpick);
     }
     int rob(vector<int>& nums) {
-        memset(dp,-1,sizeof(dp));
         int n=nums.size();
+        dp.assign(n+1,-1);
         return f(0,n,nums);
+        
     }
 };
