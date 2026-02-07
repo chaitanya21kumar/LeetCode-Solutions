68be44782e8fbd19025c7d1d49edf4e6b52ed506
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-07 22:32:34 +0530

    Time: 101 ms (44.22%) | Memory: 88.2 MB (45.73%) - LeetSync

--- 1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
+++ 1649-maximum-number-of-non-overlapping-subarrays-with-sum-equals-target/maximum-number-of-non-overlapping-subarrays-with-sum-equals-target.cpp
@@ -2,16 +2,14 @@ class Solution {
 public:
     int maxNonOverlapping(vector<int>& nums, int target) {
 
-        unordered_map<int,int> m;
         int n=nums.size();
-        int l=0,r=0,s=0;
+        int l=0,r=0,s=0,ans=0;
+        unordered_map<int,int> m;
         m[0]=-1;
-        int ans=0;
         int lidx=-1;
         while(r<n){
             s+=nums[r];
             int need=s-target;
-
             if(m.count(need)){
                 if(m[need]>=lidx){
                     ans++;
