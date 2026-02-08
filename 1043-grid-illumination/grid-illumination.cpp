01f86392bd0119ef3c216c5419fb9a4eb1ced314
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-08 20:55:02 +0530

    Time: 234 ms (40.63%) | Memory: 121.2 MB (53.65%) - LeetSync

--- 1043-grid-illumination/grid-illumination.cpp
+++ 1043-grid-illumination/grid-illumination.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     vector<int> gridIllumination(int n, vector<vector<int>>& lamps, vector<vector<int>>& queries) {
 
-        unordered_map<int,int> r,c,d,ad; // row,col,diag,anti
-        set<pair<int,int>> al; // active lamps
+        unordered_map<int,int> r,c,d,ad;
+        set<pair<int,int>> s;
         for(auto &x:lamps){
             int rr=x[0],cc=x[1];
-            if(al.find({rr,cc})==al.end()){
-                al.insert({rr,cc});
-                r[rr]++;
-                c[cc]++;
-                d[rr-cc]++;
-                ad[rr+cc]++;
+            if(s.find({rr,cc})==s.end()){
+                s.insert({rr,cc});
+                r[rr]++;c[cc]++;d[rr-cc]++;ad[rr+cc]++;
             }
         }
         vector<int> ans;
@@ -21,17 +18,15 @@ public:
             else ans.push_back(0);
             for(int i=rr-1;i<=rr+1;i++){
                 for(int j=cc-1;j<=cc+1;j++){
-                    if(al.find({i,j})!=al.end()){
-                        al.erase({i,j});
-                        r[i]--;
-                        c[j]--;
-                        d[i-j]--;
-                        ad[i+j]--;
+                    if(s.find({i,j})!=s.end()){
+                        s.erase({i,j});
+                        r[i]--;c[j]--;d[i-j]--;ad[i+j]--;
                     }
                 }
             }
         }
         return ans;
+
         
     }
 };
