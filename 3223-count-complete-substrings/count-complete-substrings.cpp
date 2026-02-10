d1a845dcc3f9ee72d3fdbbb69a3078663254474e
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-10 13:51:54 +0530

    Time: 48 ms (97.47%) | Memory: 20.7 MB (65.82%) - LeetSync

--- 3223-count-complete-substrings/count-complete-substrings.cpp
+++ 3223-count-complete-substrings/count-complete-substrings.cpp
@@ -6,29 +6,30 @@ public:
             int wl=u*k;
             if(wl>e-s+1) break;
             vector<int> f(26,0);
-            int gc=0;
+            int gc=0; // good char count
             for(int i=s;i<s+wl;i++){
-                char ch=word[i]-'a';
-                f[ch]++;
-                if(f[ch]==k) gc++;
-                else if(f[ch]==k+1) gc--;
+                int idx=word[i]-'a';
+                f[idx]++;
+                if(f[idx]==k) gc++;
+                else if(f[idx]==k+1) gc--;
             }
             if(gc==u) ans++;
             for(int i=s+wl;i<=e;i++){
-                int upcoming_char_idx=word[i]-'a';
-                int outgoing_char_idx=word[i-wl]-'a';
-                if(f[outgoing_char_idx]==k) gc--;
-                else if(f[outgoing_char_idx]==k+1) gc++;
-                f[outgoing_char_idx]--;
-                f[upcoming_char_idx]++;
-                if(f[upcoming_char_idx]==k) gc++;
-                else if(f[upcoming_char_idx]==k+1) gc--;
+                int uci=word[i]-'a'; // upcoming char idx
+                int oci=word[i-wl]-'a'; // outgoing char idx
+                if(f[oci]==k+1) gc++;
+                else if(f[oci]==k) gc--;
+                f[oci]--;
+                f[uci]++;
+                if(f[uci]==k) gc++;
+                else if(f[uci]==k+1) gc--;
                 if(gc==u) ans++;
             }
         }
         return ans;
     }
     int countCompleteSubstrings(string word, int k) {
+
         int n=word.size();
         int ans=0;
         int lidx=0;
@@ -37,8 +38,10 @@ public:
                 ans+=f(lidx,i-1,word,k);
                 lidx=i;
             }
+
         }
         ans+=f(lidx,n-1,word,k);
         return ans;
+        
     }
 };
