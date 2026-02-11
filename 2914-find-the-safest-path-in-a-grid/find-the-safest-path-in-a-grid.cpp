2ef52a9e3c54081a40cc317349d90f35ff832af5
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-11 18:40:51 +0530

    Time: 368 ms (67.61%) | Memory: 149.6 MB (57.71%) - LeetSync

--- 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
+++ 2914-find-the-safest-path-in-a-grid/find-the-safest-path-in-a-grid.cpp
@@ -4,10 +4,10 @@ public:
     int maximumSafenessFactor(vector<vector<int>>& grid) {
 
         int n=grid.size();
-        if(grid[0][0]==1 || grid[n-1][n-1]==1) return 0;
-        vector<vector<int>> dist(n,vector<int> (n,0));
-        vector<vector<int>> vis(n,vector<int> (n,0));
         queue<pair<int,int>> q;
+        vector<vector<int>> vis(n,vector<int> (n,0));
+        vector<vector<int>> dist(n,vector<int> (n,0));
+        vector<vector<int>> dirs={{0,1},{1,0},{0,-1},{-1,0}};
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
                 if(grid[i][j]==1){
@@ -17,26 +17,25 @@ public:
                 }
             }
         }
-        vector<vector<int>> dirs={{0,1},{1,0},{-1,0},{0,-1}};
         while(!q.empty()){
             auto x=q.front();
+            q.pop();
             int rr=x.first;
             int cc=x.second;
-            q.pop();
             for(auto &dir:dirs){
                 int r=rr+dir[0],c=cc+dir[1];
                 if(r>=0 && r<n && c>=0 && c<n && vis[r][c]==0){
                     vis[r][c]=1;
+                    dist[r][c]=dist[rr][cc]+1;
                     q.push({r,c});
-                    dist[r][c]=1+dist[rr][cc];
                 }
             }
         }
-        priority_queue<p> pq;
         vector<vector<int>> vis2(n,vector<int> (n,0));
+        priority_queue<p> pq;
         pq.push({dist[0][0],{0,0}});
         vis2[0][0]=1;
-        int ans=INT_MAX;
+        int ans=dist[0][0];
         while(!pq.empty()){
             auto x=pq.top();
             pq.pop();
@@ -55,5 +54,7 @@ public:
         }
         return 0;
 
+        
+        
     }
 };
