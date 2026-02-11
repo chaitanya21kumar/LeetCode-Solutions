e8afdb8b7b057b80f0b2d6e13f0623ae6316f561
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-11 13:11:54 +0530

    Time: 1392 ms (59.02%) | Memory: 455.7 MB (15.57%) - LeetSync

---
+++ 3362-find-the-median-of-the-uniqueness-array/find-the-median-of-the-uniqueness-array.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    typedef long long int ll;
+    ll f(vector<int>& nums,ll mid){
+        ll n=nums.size(),l=0,r=0,cd=0,ans=0;
+        unordered_map<ll,ll> m;
+        while(r<n){
+            if(m[nums[r]]==0) cd++;
+            m[nums[r]]++;
+            while(cd>mid){
+                m[nums[l]]--;
+                if(m[nums[l]]==0) cd--;
+                l++;
+            }
+            if(cd<=mid) ans+=(r-l+1);
+            r++;
+        }
+        return ans;
+    }
+    int medianOfUniquenessArray(vector<int>& nums) {
+
+        ll n=nums.size();
+        unordered_set<ll> s;
+        for(auto &x:nums) s.insert(x);
+        ll lo=1,hi=s.size();
+        ll tnos=(n*(n+1))/2; // total number of subarrays
+        ll r=(tnos+1)/2;
+        ll ans=hi;
+        while(lo<=hi){
+            ll mid=(lo+(hi-lo)/2);
+            ll c=f(nums,mid);
+            if(c>=r){
+                ans=mid;
+                hi=mid-1;
+            }
+            else lo=mid+1;
+        }
+        return ans;
+        
+    }
+};
