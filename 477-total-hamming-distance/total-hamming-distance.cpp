3c0cf532f2bd29a72c7455a4963fe3c302d7bf3c
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-13 16:51:54 +0530

    Time: 9 ms (76.04%) | Memory: 22.7 MB (82.35%) - LeetSync

--- 477-total-hamming-distance/total-hamming-distance.cpp
+++ 477-total-hamming-distance/total-hamming-distance.cpp
@@ -3,15 +3,21 @@ public:
     typedef long long int ll;
     int totalHammingDistance(vector<int>& nums) {
 
-        int n=nums.size();
+        ll n=nums.size();
         ll ans=0;
-        for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int xori= (nums[i] ^ nums[j]);
-                ans+=(__builtin_popcount(xori));
+        for(ll i=0;i<=32;i++){
+            ll c0=0,c1=0;
+            for(int j=0;j<n;j++){
+                ll biti=((ll)nums[j]>>i)&1;
+                if(biti==0) c0++;
+                else c1++;
             }
+            ans+=(c0*c1);
         }
         return ans;
+
+
         
+
     }
 };
