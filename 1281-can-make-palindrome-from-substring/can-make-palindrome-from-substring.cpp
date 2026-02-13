a8bd8dd25e47ad96c2b75058afb41b0878ac7a3a
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-13 08:24:08 +0530

    Time: 72 ms (40.50%) | Memory: 124 MB (40.08%) - LeetSync

--- 1281-can-make-palindrome-from-substring/can-make-palindrome-from-substring.cpp
+++ 1281-can-make-palindrome-from-substring/can-make-palindrome-from-substring.cpp
@@ -3,13 +3,11 @@ public:
     vector<bool> canMakePaliQueries(string s, vector<vector<int>>& queries) {
 
         int n=s.size();
-        vector<vector<int>> v(26,vector<int> (n,0));
+        vector<vector<int>> v(26,vector<int> (n));
         for(int i=0;i<26;i++){
             for(int j=0;j<n;j++){
-                int cci=( (s[j]-'a')==i ? 1 : 0 );
-                if(j==0){
-                    v[i][j]=cci;
-                }
+                int cci=(s[j]-'a'==i?1:0);
+                if(j==0) v[i][j]=cci;
                 else v[i][j]=v[i][j-1]+cci;
             }
         }
@@ -19,11 +17,13 @@ public:
             int oc=0;
             for(int i=0;i<26;i++){
                 int c=0;
-                if(l==0) c=v[i][r];
+                if(l==0){
+                    c=v[i][r];
+                }
                 else c=v[i][r]-v[i][l-1];
                 if(c%2!=0) oc++;
             }
-            if((oc/2)<=k) ans.push_back(true);
+            if(oc/2<=k) ans.push_back(true);
             else ans.push_back(false);
         }
         return ans;
