4db6e31863a992143ed6138fc12d89d9ecfac617
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-21 17:02:47 +0530

    Time: 50 ms (34.04%) | Memory: 11.7 MB (15.53%) - LeetSync

---
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int f(int i,int d,int n,vector<int>& jd,vector<vector<int>> &dp){
+        if(d==1){
+            int ans=INT_MIN;
+            for(int j=i;j<n;j++){
+                ans=max(ans,jd[j]);
+            }
+            return ans;
+        }
+        if(dp[i][d]!=-1) return dp[i][d];
+        int ans=INT_MAX;
+        int cur_ans=jd[i];
+        for(int j=i;j<=n-d;j++){
+            cur_ans=max(cur_ans,jd[j]);
+            ans=min(ans,cur_ans+f(j+1,d-1,n,jd,dp));
+        }
+        return dp[i][d]=ans;
+
+    }
+    int minDifficulty(vector<int>& jd, int d) {
+
+        int n=jd.size();
+        if(d>n) return -1;
+        vector<vector<int>> dp(n+10,vector<int> (d+10,-1));
+        return f(0,d,n,jd,dp);
+        
+    }
+};
