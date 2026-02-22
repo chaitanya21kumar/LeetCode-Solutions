4b2d1bf73f3a3034993343c0e75329f2717e6a11
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-22 15:59:08 +0530

    Time: 0 ms (100.00%) | Memory: 14.3 MB (10.70%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -7,17 +7,13 @@ public:
         int lo=0,hi=n-1;
         while(lo<=hi){
             int mid=(lo+(hi-lo)/2);
-            if(nums[mid]>=nums[lo] && nums[mid]<=nums[hi]){
-                ans=min(ans,nums[lo]);
-                hi=mid-1;
-            }
-            else if(nums[lo]>nums[mid]){
-                ans=min(ans,nums[mid]);
-                hi=mid-1;
-            }
-            else if(nums[lo]<=nums[mid] && nums[mid]>nums[hi]){
+            if(nums[mid]>=nums[lo]){
                 ans=min(ans,nums[lo]);
                 lo=mid+1;
+            }
+            else if(nums[lo]>=nums[mid]){
+                ans=min(ans,nums[mid]);
+                hi=mid-1;
             }
         }
         return ans;
