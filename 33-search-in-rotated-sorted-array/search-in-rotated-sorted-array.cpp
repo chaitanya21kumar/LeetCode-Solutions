12d5d837a85ee6865824cc6bc47b1c299b76e72c
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-22 16:15:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.2 MB (66.71%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -4,22 +4,23 @@ public:
 
         int n=nums.size();
         int lo=0,hi=n-1;
-
         while(lo<=hi){
             int mid=(lo+(hi-lo)/2);
             if(nums[mid]==t) return mid;
             else if(nums[lo]<=nums[mid]){
-                if(t>=nums[lo] && t<=nums[mid]) hi=mid-1;
+                if(nums[lo]<=t && t<=nums[mid]){
+                    hi=mid-1;
+                }
                 else lo=mid+1;
             }
             else if(nums[mid]<=nums[hi]){
-                if(t>=nums[mid] && t<=nums[hi]) lo=mid+1;
+                if(nums[mid]<=t && t<=nums[hi]){
+                    lo=mid+1;
+                }
                 else hi=mid-1;
             }
         }
         return -1;
-
-        
         
     }
 };
