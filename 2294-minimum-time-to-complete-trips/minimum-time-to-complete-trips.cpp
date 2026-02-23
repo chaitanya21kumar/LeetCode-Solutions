4d2e6c72eb99da98e4078f187da578a397968bf7
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-02-23 15:26:41 +0530

    Time: 107 ms (5.13%) | Memory: 98.5 MB (52.76%) - LeetSync

---
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    typedef long long int ll;
+    bool possible(ll mid,vector<int>& time, int totalTrip){
+        ll x=0;
+        for(int i=0;i<time.size();i++){
+            x+=(mid/time[i]);
+        }
+        if(x>=totalTrip) return true;
+        return false;
+    }
+    long long minimumTime(vector<int>& time, int totalTrips) {
+
+        ll n=time.size();
+        ll lo=0,hi=1e14;
+        ll ans=hi;
+        while(lo<=hi){
+            ll mid=(lo+(hi-lo)/2);
+            if(possible(mid,time,totalTrips)){
+                ans=mid;
+                hi=mid-1;
+            }
+            else lo=mid+1;
+        }
+        return ans;
+        
+    }
+};
