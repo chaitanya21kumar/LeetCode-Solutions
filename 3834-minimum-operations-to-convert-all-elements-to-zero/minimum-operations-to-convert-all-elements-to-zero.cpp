5d2cb7c986d3cdefef8d9e82a40abdf21b9c8191
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-01 20:45:53 +0530

    Time: 52 ms (26.67%) | Memory: 222.1 MB (71.43%) - LeetSync

--- 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
+++ 3834-minimum-operations-to-convert-all-elements-to-zero/minimum-operations-to-convert-all-elements-to-zero.cpp
@@ -6,19 +6,13 @@ public:
         stack<int> st;
         int ops=0;
         for(int i=0;i<n;i++){
-            if(st.empty() && nums[i]!=0){
-                ops++;
-                st.push(nums[i]);
-                continue;
-            }
-            while(!st.empty() && st.top()>nums[i]){
-                st.pop();
-            }
+            while(!st.empty() && st.top()>nums[i]) st.pop();
             if(nums[i]!=0){
-                if(st.empty()) ops++;
-                else if(nums[i]!=st.top()) ops++;
+                if(st.empty() || (st.top()!=nums[i])){
+                    st.push(nums[i]);
+                    ops++;
+                }
             }
-            st.push(nums[i]);
         }
         return ops;
         
