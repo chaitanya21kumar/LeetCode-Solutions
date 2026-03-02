76a8977656ddfa45d4629b28ceacddbee8dba289
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-02 21:37:51 +0530

    Time: 3 ms (33.22%) | Memory: 12.1 MB (10.93%) - LeetSync

--- 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
+++ 2021-remove-all-occurrences-of-a-substring/remove-all-occurrences-of-a-substring.cpp
@@ -1,28 +1,13 @@
 class Solution {
 public:
-    bool check(stack<char> st,string &part,int n){
-        for(int i=n-1;i>=0;i--){
-            if(st.top()!=part[i]) return false;
-            st.pop();
-        }
-        return true;
-    }
     string removeOccurrences(string s, string part) {
 
-        int m=s.size(),n=part.size();
-        stack<char> st;
-        for(int i=0;i<m;i++){
-            st.push(s[i]);
-            if(st.size()>=n && check(st,part,n)==true){
-                for(int j=0;j<n;j++) st.pop();
-            }
-        } 
+        int n=part.size();
         string ans="";
-        while(!st.empty()){
-            ans+=st.top();
-            st.pop();
+        for(int i=0;i<s.size();i++){
+            ans+=s[i];
+            if(ans.size()>=n && ans.substr(ans.size()-n)==part) ans.erase(ans.size()-n);
         }
-        reverse(ans.begin(),ans.end());
         return ans;
         
     }
