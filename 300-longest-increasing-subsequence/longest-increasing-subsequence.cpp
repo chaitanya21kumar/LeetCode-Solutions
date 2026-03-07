61e3156c2f0345ce636bf9ba1fa1acdf7cfd28af
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-07 23:41:45 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (94.80%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -1,20 +1,17 @@
 class Solution {
 public:
-    int f(int i,int pidx,int n,vector<int>& nums,vector<vector<int>> &dp){
-        if(i>=n) return 0;
-        if(dp[i][pidx+1]!=-1) return dp[i][pidx+1];
-        int pick=INT_MIN,notpick=INT_MIN;
-        notpick=f(i+1,pidx,n,nums,dp);
-        if(pidx==-1 || nums[i]>nums[pidx]){
-            pick=1+f(i+1,i,n,nums,dp);
-        }
-        return dp[i][pidx+1]=max(pick,notpick);
-    }
     int lengthOfLIS(vector<int>& nums) {
 
         int n=nums.size();
-        vector<vector<int>> dp(n+10,vector<int> (n+10,-1));
-        return f(0,-1,n,nums,dp);
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            auto x=lower_bound(ans.begin(),ans.end(),nums[i]);
+            if(x==ans.end()){
+                ans.push_back(nums[i]);
+            }
+            else ans[x-ans.begin()]=nums[i];
+        }
+        return (int)ans.size();
         
     }
 };
