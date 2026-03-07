0e227b632362a212103cb4dbc9317d3af8bddd48
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-07 21:49:17 +0530

    Time: 39 ms (85.95%) | Memory: 65.5 MB (75.73%) - LeetSync

--- 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.cpp
+++ 1554-minimum-time-to-collect-all-apples-in-a-tree/minimum-time-to-collect-all-apples-in-a-tree.cpp
@@ -1,26 +1,25 @@
 class Solution {
 public:
-    unordered_map<int,vector<int>> adj;
-    int f(int cur,int par,int n,vector<bool>& hasApple){
-
+    int f(int cur,int par,vector<vector<int>> &adj,vector<bool>& hasApple){
+        
         int time=0;
-        for(auto &child:adj[cur]){
-            if(child==par) continue;
-            int time_from_this_child=f(child,cur,n,hasApple);
-            if(time_from_this_child>0 || hasApple[child]) time+=2+time_from_this_child;
+        for(auto &x:adj[cur]){
+            if(x==par) continue;
+            int time_from_this_child=f(x,cur,adj,hasApple);
+            if(time_from_this_child>0 || hasApple[x]) time+=2+time_from_this_child;
         }
         return time;
     }
     int minTime(int n, vector<vector<int>>& edges, vector<bool>& hasApple) {
 
-        for(auto x:edges){
+        vector<vector<int>> adj(n);
+        for(auto &x:edges){
             int u=x[0];
             int v=x[1];
             adj[u].push_back(v);
             adj[v].push_back(u);
         }
-
-        return f(0,-1,n,hasApple);
-
+        return f(0,-1,adj,hasApple);
+        
     }
 };
