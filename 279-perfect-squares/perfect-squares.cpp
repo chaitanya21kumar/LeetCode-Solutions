29822edc8f205dd535238c1edfeb8ccfbd60d9ce
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-08 17:44:43 +0530

    Time: 75 ms (42.47%) | Memory: 13.9 MB (36.43%) - LeetSync

--- 279-perfect-squares/perfect-squares.cpp
+++ 279-perfect-squares/perfect-squares.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
-    typedef long long int ll;
-    int f(int i,int n,int num,vector<vector<int>> &dp,vector<ll> &ps){
-
-        if(num==0) return 0;
-        if(i>=n) return 1e9;
-        if(dp[i][num]!=-1) return dp[i][num];
-        int pick=1e9,notpick=1e9;
-        if(num>=ps[i]){
-            pick=1+f(i,n,num-ps[i],dp,ps);
+    int f(int n, vector<int> &dp){
+        if(n==0) return 0;
+        if(dp[n]!=-1) return dp[n];
+        int ans=1e9;
+        for(int i=1;i*i<=n;i++){
+            ans=min(ans,1+f(n-i*i,dp));
         }
-        notpick=f(i+1,n,num,dp,ps);
-        return dp[i][num]=min(pick,notpick);
-
+        return dp[n]=ans;
     }
     int numSquares(int n) {
-        
-        vector<ll> ps;
-        for(int i=1;i*i<=n;i++){
-            ps.push_back(i*i);
-        }
-        vector<vector<int>> dp((int)ps.size()+10,vector<int> (n+10,-1));
-        return f(0,ps.size(),n,dp,ps);
 
+        vector<int> dp(n+10,-1);
+        return f(n,dp);
+        
     }
 };
