6041dffc00837567234e46bb0e431b44b4558e5e
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-09 16:10:21 +0530

    Time: 55 ms (28.98%) | Memory: 11.4 MB (17.90%) - LeetSync

--- 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
+++ 1457-minimum-difficulty-of-a-job-schedule/minimum-difficulty-of-a-job-schedule.cpp
@@ -1,28 +1,26 @@
 class Solution {
 public:
-    int f(int i,int d,int n,vector<int>& jd,vector<vector<int>> &dp){
+    int f(int i,int d,int n,vector<vector<int>> &dp,vector<int>& jd){
         if(d==1){
             int ans=INT_MIN;
-            for(int j=i;j<n;j++){
-                ans=max(ans,jd[j]);
-            }
+            for(int j=i;j<n;j++) ans=max(ans,jd[j]);
             return ans;
         }
-        if(dp[i][d]!=-1) return dp[i][d]; 
-        int ans=INT_MAX;
-        int cur_ans=INT_MIN;
+        if(dp[i][d]!=-1) return dp[i][d];
+        int mn=INT_MIN;
+        int ansi=1e9;
         for(int j=i;j<=n-d;j++){
-            cur_ans=max(cur_ans,jd[j]);
-            ans=min(ans,cur_ans+f(j+1,d-1,n,jd,dp));
+            mn=max(mn,jd[j]);
+            ansi=min(ansi,mn+f(j+1,d-1,n,dp,jd));
         }
-        return dp[i][d]=ans;
+        return dp[i][d]=ansi;
     }
     int minDifficulty(vector<int>& jd, int d) {
 
         int n=jd.size();
-        if(d>n) return -1;
         vector<vector<int>> dp(n+10,vector<int> (d+10,-1));
-        return f(0,d,n,jd,dp);
+        if(d>n) return -1;
+        return f(0,d,n,dp,jd);
         
     }
 };
