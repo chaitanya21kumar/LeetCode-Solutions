6c1a845947796ce299fe4c2587552801e8575e50
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-20 19:53:22 +0530

    Time: 297 ms (16.28%) | Memory: 361.4 MB (9.93%) - LeetSync

--- 3902-maximize-spanning-tree-stability-with-upgrades/maximize-spanning-tree-stability-with-upgrades.cpp
+++ 3902-maximize-spanning-tree-stability-with-upgrades/maximize-spanning-tree-stability-with-upgrades.cpp
@@ -1,22 +1,21 @@
 struct DSU{
 
     vector<int> parent,rank;
-
     DSU(int n){
         parent.resize(n);
         rank.resize(n,1);
         for(int i=0;i<n;i++) parent[i]=i;
     }
 
-    int find(int u){
-        if(parent[u]==u) return u;
-        return parent[u]=find(parent[u]);
+    int find(int i){
+        if(parent[i]==i) return i;
+        return parent[i]=find(parent[i]);
     }
 
     bool unite(int u,int v){
-        int ulpu=find(u);
-        int ulpv=find(v);
-        
+        int ulpu=find(u); // ultimate parent of u
+        int ulpv=find(v); // ultimate parent of v
+
         if(ulpu==ulpv) return false;
         if(rank[ulpu]>rank[ulpv]){
             parent[ulpv]=ulpu;
@@ -32,45 +31,46 @@ struct DSU{
     }
 
 };
-
+bool possible(int n,int mid,vector<vector<int>>& edges,int k){
+    DSU dsu(n);
+    vector<vector<int>> upgrades;
+    for(auto &x:edges){
+        int u=x[0],v=x[1],s=x[2],m=x[3];
+        if(m==1){
+            if(s<mid) return false;
+            dsu.unite(u,v);
+        }
+        else{
+            if(s>=mid){
+                dsu.unite(u,v);
+            }
+            else if(2*s>=mid){
+                upgrades.push_back({u,v});
+            }
+        }
+    }
+    for(auto &x:upgrades){
+        int u=x[0],v=x[1];
+        if(dsu.find(u)!=dsu.find(v)){
+            if(k<=0) return false;
+            dsu.unite(u,v);
+            k--;
+        }
+    }
+    int root=dsu.find(0);
+    for(int i=1;i<n;i++){
+        if(dsu.find(i)!=root) return false;
+    }
+    return true;
+}
 class Solution {
 public:
-    bool possible(int n,vector<vector<int>>& edges,int k,int mid){
-        DSU dsu(n);
-        vector<vector<int>> upgrades;
-        
-        for(auto &x:edges){
-            int u=x[0],v=x[1],s=x[2],m=x[3];
-            if(m==1){
-                if(s<mid) return false;
-                dsu.unite(u,v);
-            }
-            else{
-                if(s>=mid) dsu.unite(u,v);
-                else if(2*s>=mid){
-                    upgrades.push_back({u,v});
-                }
-            }
-        }
-        for(auto &x:upgrades){
-            int u=x[0],v=x[1];
-            if(dsu.find(u)!=dsu.find(v)){
-                if(k<=0) return false;
-                dsu.unite(u,v);
-                k--;
-            }
-        }
-        int root=dsu.find(0);
-        for(int i=1;i<n;i++){
-            if(dsu.find(i)!=root) return false;
-        }
-        return true;
 
-    }
     int maxStability(int n, vector<vector<int>>& edges, int k) {
 
-        int lo=1,hi=2*1e5;
         int ans=-1;
+        int lo=1;
+        int hi=2*1e5;
         DSU dsu(n);
         for(auto &x:edges){
             int u=x[0],v=x[1],s=x[2],m=x[3];
@@ -79,9 +79,10 @@ public:
                 dsu.unite(u,v);
             }
         }
+
         while(lo<=hi){
             int mid=(lo+(hi-lo)/2);
-            if(possible(n,edges,k,mid)){
+            if(possible(n,mid,edges,k)){
                 ans=mid;
                 lo=mid+1;
             }
