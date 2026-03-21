7c6073d8ce6881b5c6781c4a29e9bb7dd161d6f0
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-21 23:11:35 +0530

    Time: 79 ms (40.43%) | Memory: 93.9 MB (62.10%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+
+        int n=nums.size();
+        unordered_set<int> s;
+        for(int i=0;i<n;i++){
+            if(s.count(nums[i])) return true;
+            s.insert(nums[i]);
+            if(s.size()>k) s.erase(nums[i-k]);
+        }
+        return false;
+        
+    }
+};
