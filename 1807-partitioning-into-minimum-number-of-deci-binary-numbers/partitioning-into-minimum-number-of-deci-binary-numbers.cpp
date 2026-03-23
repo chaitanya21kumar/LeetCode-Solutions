c4375c7ae3cb0b30d691619539b53bc19aa86e52
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-24 00:07:53 +0530

    Time: 0 ms (100.00%) | Memory: 16.5 MB (80.65%) - LeetSync

---
+++ 1807-partitioning-into-minimum-number-of-deci-binary-numbers/partitioning-into-minimum-number-of-deci-binary-numbers.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minPartitions(string n) {
+
+        int mx=0;
+        for(int i=0;i<n.size();i++){
+            int x=n[i]-'0';
+            mx=max(mx,x);
+        }
+        return mx;
+        
+    }
+};
