31c9b095433947e4aa8ff55b1c231ad531bfa893
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-24 21:18:58 +0530

    Time: 133 ms (28.57%) | Memory: 224.4 MB (59.91%) - LeetSync

---
+++ 3647-zero-array-transformation-iii/zero-array-transformation-iii.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+
+        int n=nums.size();
+        priority_queue<int> a; // max heap of available
+        priority_queue<int,vector<int>,greater<int>> u; // min heap of used
+        sort(queries.begin(),queries.end());
+
+        int qi=0; // queries idx
+        int uc=0; // used count
+        int qs=queries.size();
+
+        for(int i=0;i<n;i++){
+            while(qi<qs && queries[qi][0]==i){
+                a.push(queries[qi][1]);
+                qi++;
+            }
+
+            nums[i]-=u.size();
+
+            while(nums[i]>0 && !a.empty() && a.top()>=i){
+                u.push(a.top());
+                a.pop();
+                nums[i]--;
+                uc++;
+            }
+
+            if(nums[i]>0) return -1;
+
+            while(!u.empty() && u.top()==i) u.pop();
+        }
+
+        return qs-uc;
+        
+    }
+};
