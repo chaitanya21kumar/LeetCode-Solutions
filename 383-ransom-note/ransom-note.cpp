8f239b93adc7e96dc619ce0b40e8c8f90c7dd84f
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-03-29 20:07:49 +0530

    Time: 7 ms (24.21%) | Memory: 12 MB (28.72%) - LeetSync

---
+++ 383-ransom-note/ransom-note.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool canConstruct(string ransomNote, string magazine) {
+
+        unordered_map<char,int> m;
+        for(int i=0;i<ransomNote.size();i++){
+            m[ransomNote[i]]++;
+        }
+        for(int i=0;i<magazine.size();i++){
+            m[magazine[i]]--;
+        }
+        for(auto &x:m){
+            if(x.second>0) return false;
+        }
+        return true;
+        
+    }
+};
