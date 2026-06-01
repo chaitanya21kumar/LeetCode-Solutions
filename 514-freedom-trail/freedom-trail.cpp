9a614b4accca39c8eeaf01aaecfb2764e61d7a0a
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-06-01 12:14:55 +0530

    Time: 130 ms (16.60%) | Memory: 32.9 MB (15.18%) - LeetSync

--- 514-freedom-trail/freedom-trail.cpp
+++ 514-freedom-trail/freedom-trail.cpp
@@ -1,30 +1,42 @@
 class Solution {
 public:
-    int f1(int ri,int ki,int n){
-
+    int f(int ri,int ki,int n){
         int ans=INT_MAX;
         int ans1=abs(ri-ki);
         int ans2=n-ans1;
         return ans=min(ans1,ans2);
-
-    }
-    int f(int ri,int ki,int n,string &ring, string &key,vector<vector<int>> &dp){
-        if(ki>=n) return 0;
-        if(dp[ri][ki]!=-1) return dp[ri][ki];
-        int ans=INT_MAX;
-        for(int i=0;i<ring.length();i++){
-            if(ring[i]==key[ki]){
-                int temp=f1(ri,i,ring.length())+f(i,ki+1,n,ring,key,dp)+1;
-                ans=min(ans,temp);
-            }
-        }
-        return dp[ri][ki]=ans;
     }
     int findRotateSteps(string ring, string key) {
 
-        int n=key.length();
-        vector<vector<int>> dp(ring.length()+10,vector<int> (n+10,-1));
-        return f(0,0,n,ring,key,dp);
+        int nr=ring.length();
+        int nk=key.length();
+
+        unordered_map<char,vector<int>> m;
+        for(int i=0;i<nr;i++){
+            m[ring[i]].push_back(i);
+        }
+        priority_queue<vector<int>,vector<vector<int>>,greater<vector<int>>> pq;
+        pq.push({0,0,0});
+
+        set<pair<int,int>> st;
+
+        while(!pq.empty()){
+            auto x=pq.top();
+            int s=x[0];
+            int ri=x[1];
+            int ki=x[2];
+            pq.pop();
+
+            if(st.find({ri,ki})!=st.end()) continue;
+            st.insert({ri,ki});
+            if(ki>=nk) return s+nk;
+
+            for(auto &y:m[key[ki]]){
+                int steps=f(ri,y,nr);
+                pq.push({s+steps,y,ki+1,});
+            }
+        }
         
+        return 0;
     }
 };
