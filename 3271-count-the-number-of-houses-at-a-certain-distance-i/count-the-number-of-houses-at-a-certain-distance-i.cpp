73ed781921869098f66b44701e28216d9fd0e388
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-06-19 00:25:39 +0530

    Time: 71 ms (55.74%) | Memory: 30.4 MB (24.04%) - LeetSync

--- 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
+++ 3271-count-the-number-of-houses-at-a-certain-distance-i/count-the-number-of-houses-at-a-certain-distance-i.cpp
@@ -1,19 +1,46 @@
 class Solution {
 public:
+    typedef pair<int,int> p;
+    typedef long long int ll;
     vector<int> countOfPairs(int n, int x, int y) {
 
         vector<int> ans(n);
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=n;j++){
-                if(i==j) continue;
-                int d1=abs(i-j);
-                int d2=abs(i-x)+1+abs(y-j);
-                int d3=abs(i-y)+1+abs(x-j);
-                int d=min({d1,d2,d3});
-                ans[d-1]++;
+        vector<vector<ll>> adj(n+1);
+        for(ll i=1;i<n;i++){
+            adj[i].push_back(i+1);
+            adj[i+1].push_back(i);
+        }
+        if(x!=y){
+            adj[x].push_back(y);
+            adj[y].push_back(x);
+        }
+        for(ll i=1;i<=n;i++){
+            vector<ll> dist(n+1,1e7);
+            priority_queue<p,vector<p>,greater<p>> pq;
+            pq.push({0,i});
+            dist[i]=0;
+            while(!pq.empty()){
+                auto x=pq.top();
+                pq.pop();
+                ll d=x.first;
+                ll node=x.second;
+                if(dist[node]<d) continue;
+                for(auto &y:adj[node]){
+                    if(dist[y]>d+1){
+                        dist[y]=d+1;
+                        pq.push({dist[y],y});
+                    }
+                }
+            }
+            for(int k=1;k<=n;k++){
+                if(k!=i && dist[k]!=1e7){
+                    ans[dist[k]-1]++;
+                }
             }
         }
         return ans;
+
+
         
     }
 };
