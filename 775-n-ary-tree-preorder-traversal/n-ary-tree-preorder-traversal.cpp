47fb896ffa2539dd218b1115ac71680ce098b4c2
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-08-03 23:07:39 +0530

    Time: 13 ms (56.01%) | Memory: 15.5 MB (69.04%) - LeetSync

---
+++ 775-n-ary-tree-preorder-traversal/n-ary-tree-preorder-traversal.cpp
@@ -0,0 +1,37 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> children;
+
+    Node() {}
+
+    Node(int _val) {
+        val = _val;
+    }
+
+    Node(int _val, vector<Node*> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+public:
+    vector<int> ans;
+    void f(Node* root){
+        if(!root) return;
+        ans.push_back(root->val);
+        for(auto &x:root->children){
+            f(x);
+        }
+    }
+    vector<int> preorder(Node* root) {
+
+        f(root);
+        return ans;
+
+    }
+};
