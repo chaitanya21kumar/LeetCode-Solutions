45f1f0f72e58602b708affba252b8b594a4f1aee
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-08-12 16:11:17 +0530

    Time: 7 ms (60.64%) | Memory: 14.7 MB (11.65%) - LeetSync

--- 72-edit-distance/edit-distance.cpp
+++ 72-edit-distance/edit-distance.cpp
@@ -1,32 +1,29 @@
 class Solution {
 public:
-    int dp[550][550];
-    int f(int i,int j,int n1,int n2,string &s1,string &s2){
-        if(i==n1 && j<n2) return n2-j;
-        if(j==n2 && i<n1) return n1-i;
-        if(j==n2 && i==n1) return 0;
+    int f(int i,int j,int n1,int n2,vector<vector<int>> &dp,string &w1,string &w2){
+        if(i==n1 && j==n2) return 0;
+        if(i==n1) return n2-j;
+        if(j==n2) return n1-i;
+
         if(dp[i][j]!=-1) return dp[i][j];
-
-        int rem=INT_MAX,rep=INT_MAX,ins=INT_MAX;
-        if(s1[i]==s2[j]){
-            rem=f(i+1,j+1,n1,n2,s1,s2);
-        }
+        if(w1[i]==w2[j]) dp[i][j]=f(i+1,j+1,n1,n2,dp,w1,w2);
         else{
-            rem=1+f(i+1,j,n1,n2,s1,s2);
-            rep=1+f(i+1,j+1,n1,n2,s1,s2);
-            ins=1+f(i,j+1,n1,n2,s1,s2);
+            int ins=1+f(i,j+1,n1,n2,dp,w1,w2);
+            int del=1+f(i+1,j,n1,n2,dp,w1,w2);
+            int rep=1+f(i+1,j+1,n1,n2,dp,w1,w2);
+            dp[i][j]=min({ins,del,rep});
         }
-
-        return dp[i][j]=min({rem,rep,ins});
-
+        
+        return dp[i][j];
+        
     }
     int minDistance(string word1, string word2) {
 
-        int n1=word1.size(),n2=word2.size();
-        memset(dp,-1,sizeof(dp));
+        int n1=word1.size();
+        int n2=word2.size();
 
-        return f(0,0,n1,n2,word1,word2);
-
+        vector<vector<int>> dp(n1+1,vector<int> (n2+1,-1));
+        return f(0,0,n1,n2,dp,word1,word2);
         
     }
 };
