4f113f57fc5a7ed9e4846bb77df64e742b6f8569
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-08-20 22:39:04 +0530

    Time: 232 ms (59.31%) | Memory: 10.7 MB (55.53%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,35 +1,38 @@
 class Solution {
-public: 
-    int dr[4]={-1,0,1,0};
-    int dc[4]={0,1,0,-1};
-    int dfs(int i,int j,int k,int ws,int m,int n,vector<vector<char>>& board,string &word){
-        if(k==ws) return true;
-        char org=board[i][j];
-        board[i][j]='#';
+public:
+    vector<vector<int>> dirs={{0,1},{1,0},{-1,0},{0,-1}};
+    bool f(int i,int j,int m,int n,int idx,vector<vector<char>>& board,string &word){
 
-        for(int l=0;l<4;l++){
-            int r=i+dr[l];
-            int c=j+dc[l];
-            if(r>=0 && r<m && c>=0 && c<n && board[r][c]==word[k] && board[r][c]!='#'){
-                if(dfs(r,c,k+1,ws,m,n,board,word)) return true;
+        if(board[i][j]!=word[idx]) return false;
+        if(idx==word.size()-1) return true;
+        char ch=board[i][j];
+        bool ans=false;
+        board[i][j]='$';
+        for(auto &x:dirs){
+            int ii=x[0];
+            int jj=x[1];
+            int r=i+ii;
+            int c=j+jj;
+            if(r>=0 && r<m && c>=0 && c<n && board[r][c]!='$'){
+                ans=ans|f(r,c,m,n,idx+1,board,word);
             }
         }
+        board[i][j]=ch;
+        return ans;
 
-        board[i][j]=org;
-        return false;
     }
     bool exist(vector<vector<char>>& board, string word) {
 
-        int m=board.size(),n=board[0].size(),ws=word.size();
+        int m=board.size();
+        int n=board[0].size();
 
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
                 if(board[i][j]==word[0]){
-                    if(dfs(i,j,1,ws,m,n,board,word)) return true;
+                    if(f(i,j,m,n,0,board,word)) return true;
                 }
             }
         }
-
         return false;
         
     }
