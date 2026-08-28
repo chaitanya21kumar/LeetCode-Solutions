0b71c4e323d1522cfed2ace16a12182c966055a4
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-08-28 22:14:22 +0530

    Time: 3 ms (41.30%) | Memory: 12.9 MB (39.35%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+
+        int n1=nums1.size();
+        int n2=nums2.size();
+        stack<int> st;
+        vector<int> nge(n2,-1);
+        for(int i=n2-1;i>=0;i--){
+            while(!st.empty() && st.top()<=nums2[i]){
+                st.pop();
+            }
+            if(!st.empty()){
+                nge[i]=st.top();
+            }
+            st.push(nums2[i]);
+        }
+        vector<int> ans(n1,-1);
+        unordered_map<int,int> m;
+        for(int i=0;i<n2;i++){
+            m[nums2[i]]=nge[i];
+        }
+        for(int i=0;i<n1;i++){
+            ans[i]=m[nums1[i]];
+        }
+        return ans;
+        
+    }
+};
