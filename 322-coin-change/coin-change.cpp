daeced20278108a6a05c16da1dd706b79b31aa8f
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-09-01 19:29:17 +0530

    Time: 71 ms (6.80%) | Memory: 91 MB (5.01%) - LeetSync

--- 322-coin-change/coin-change.cpp
+++ 322-coin-change/coin-change.cpp
@@ -1,25 +1,32 @@
 class Solution {
 public:
-    int f(int i,int amt,int n,vector<int>& coins,vector<vector<int>> &dp){
-
-        if(i>=n && amt!=0) return 1e9;
-        if(amt==0) return 0;
-        if(dp[i][amt]!=-1) return dp[i][amt];
-        int pick=1e9;
-        if(amt>=coins[i]){
-            pick=1+f(i,amt-coins[i],n,coins,dp);
+    int f(int i,int n,vector<int>& coins,int amount,vector<vector<int>> &dp){
+        if(i==n){
+            if(amount==0) return 0;
+            return INT_MAX;
         }
-        int notpick=f(i+1,amt,n,coins,dp);
-        return dp[i][amt]=min(pick,notpick);
-
+        if(dp[i][amount]!=-1) return dp[i][amount];
+        int pick=INT_MAX;
+        int notpick=INT_MAX;
+        if(amount>=coins[i]){
+            int x=f(i,n,coins,amount-coins[i],dp);
+            if(x!=INT_MAX){
+                pick=1+x;
+            }
+        }
+        int y=f(i+1,n,coins,amount,dp);
+        if(y!=INT_MAX){
+            notpick=y;
+        }
+        return dp[i][amount]=min(pick,notpick);
     }
     int coinChange(vector<int>& coins, int amount) {
 
         int n=coins.size();
         vector<vector<int>> dp(n+10,vector<int> (amount+10,-1));
-        int ans=f(0,amount,n,coins,dp);
-        if(ans>=1e9) return -1;
-        return ans;
+        int x=f(0,n,coins,amount,dp);
+        if(x==INT_MAX) return -1;
+        return x;
         
     }
 };
