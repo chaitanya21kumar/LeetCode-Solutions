e883c40aaa356b597691fe16055d07cb91d2b62e
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-09-05 03:51:47 +0530

    Time: 4 ms (52.83%) | Memory: 16 MB (5.75%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,17 +1,22 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int t) {
+    vector<int> twoSum(vector<int>& nums, int target) {
 
         int n=nums.size();
         unordered_map<int,int> m;
-
         for(int i=0;i<n;i++){
-            int x=t-nums[i];
-            if(m.find(x)!=m.end()) return {m[x],i};
             m[nums[i]]=i;
         }
-        
-        return {};
+        vector<int> ans;
+        for(int i=0;i<n;i++){
+            int x=target-nums[i];
+            if(m.find(x)!=m.end() && i!=m[x]){
+                ans.push_back(i);
+                ans.push_back(m[x]);
+                break;
+            }
+        }
+        return ans;
         
     }
 };
