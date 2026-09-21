72798b4645a1c81b963a6d2e6a4a10e5dd416e1f
Chaitanya Kumar <multiversesyndrome@gmail.com>
2026-09-22 05:03:49 +0530

    Time: 9 ms (13.48%) | Memory: 37.1 MB (9.96%) - LeetSync

--- 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.cpp
+++ 4418-number-of-intersecting-interval-pairs-i/number-of-intersecting-interval-pairs-i.cpp
@@ -2,17 +2,18 @@ class Solution {
 public:
     int countIntersectingIntervals(vector<vector<int>>& intervals) {
 
-        sort(intervals.begin(),intervals.end());
-        int ans=0;
         int n=intervals.size();
+        sort(intervals.begin(),intervals.end());
+        vector<int> s(n);
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                int s1=intervals[i][0];
-                int e1=intervals[i][1];
-                int s2=intervals[j][0];
-                int e2=intervals[j][1];
-                if(e1>=s2) ans++;
-            }
+            s[i]=intervals[i][0];
+        }
+        int ans=0;
+        for(int i=0;i<n;i++){
+            int e=intervals[i][1];
+            auto it=upper_bound(s.begin()+i+1,s.end(),e);
+            int d=it-(s.begin()+i+1);
+            ans+=d;
         }
         return ans;
         
